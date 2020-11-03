1cf50e9aa729942728704ec16d549e99d9efcb78
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 17:32:30 -0800

    added SQL URL?  I think..., for real this time

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ import fetch from 'superagent';
 
 app.use(cors());
 
-API_URL = 'postgres://mukrixtxqkosfi:66be4f096756182eba49c28c7862dbe96dfb43d48cc9c1820d167cab0f9848d3@ec2-54-224-124-241.compute-1.amazonaws.com:5432/d3qirmhfvbulbi';
+SQL_URL = 'postgres://mukrixtxqkosfi:66be4f096756182eba49c28c7862dbe96dfb43d48cc9c1820d167cab0f9848d3@ec2-54-224-124-241.compute-1.amazonaws.com:5432/d3qirmhfvbulbi';
 
 app.get('/', async (req, res) => {
-    const returned_data = await fetch.get(API_URL);
+    const returned_data = await fetch.get(SQL_URL);
     res.json(returned_data);
 
 })
